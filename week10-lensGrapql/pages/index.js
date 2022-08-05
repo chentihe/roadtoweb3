a7d588d834f959beb426be67994aa9f13ed351ff
Tihe <tihechen@gmail.com>
2022-08-05 17:45:36 +0800

    week10 done

---
+++ week10-lensGrapql/pages/index.js
@@ -0,0 +1,105 @@
+import { gql, useQuery } from '@apollo/client'
+import Profile from '../components/Profile';
+
+const recommendProfiles = gql`
+  query RecommendedProfiles {
+    recommendedProfiles {
+          id
+        name
+        bio
+        attributes {
+          displayType
+          traitType
+          key
+          value
+        }
+          followNftAddress
+        metadata
+        isDefault
+        picture {
+          ... on NftImage {
+            contractAddress
+            tokenId
+            uri
+            verified
+          }
+          ... on MediaSet {
+            original {
+              url
+              mimeType
+            }
+          }
+          __typename
+        }
+        handle
+        coverPicture {
+          ... on NftImage {
+            contractAddress
+            tokenId
+            uri
+            verified
+          }
+          ... on MediaSet {
+            original {
+              url
+              mimeType
+            }
+          }
+          __typename
+        }
+        ownedBy
+        dispatcher {
+          address
+          canUseRelay
+        }
+        stats {
+          totalFollowers
+          totalFollowing
+          totalPosts
+          totalComments
+          totalMirrors
+          totalPublications
+          totalCollects
+        }
+        followModule {
+          ... on FeeFollowModuleSettings {
+            type
+            amount {
+              asset {
+                symbol
+                name
+                decimals
+                address
+              }
+              value
+            }
+            recipient
+          }
+          ... on ProfileFollowModuleSettings {
+          type
+          }
+          ... on RevertFollowModuleSettings {
+          type
+          }
+        }
+    }
+  }
+`;
+
+export default function Home() {
+  const {loading, error, data} = useQuery(recommendProfiles);
+
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+
+  return (
+    <>
+      {data.recommendedProfiles.map((profile, index) => {
+        console.log(`Profile ${index}:`, profile);
+        return (
+          <Profile profile={profile} key={index} />
+        );
+      })}
+    </>
+  )
+}
